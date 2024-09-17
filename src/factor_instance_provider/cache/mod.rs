aaa3959168c420a28eace70bf9a6c6887a91767a
Alexander Cyon <alex.cyon@gmail.com>
2024-09-17 18:44:55 +0200

    refactor more

---
+++ src/factor_instance_provider/cache/mod.rs
@@ -0,0 +1,3 @@
+mod is_pre_derived_keys_cache;
+
+pub use is_pre_derived_keys_cache::*;
--- src/factor_instance_provider/mod.rs
+++ src/factor_instance_provider/mod.rs
@@ -2,6 +2,7 @@ mod derivation_request;
 #[allow(clippy::module_inception)]
 mod factor_instance_provider;
 
+mod cache;
 mod gateway;
 mod next_derivation_peek_outcome;
 mod profile_extensions;
@@ -10,6 +11,7 @@ mod unfulfillable_request;
 mod unfulfillable_request_reason;
 mod unfulfillable_requests;
 
+pub use cache::*;
 pub use derivation_request::*;
 pub use factor_instance_provider::*;
 pub use gateway::*;
