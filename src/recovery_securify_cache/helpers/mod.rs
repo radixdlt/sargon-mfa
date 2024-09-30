30f5f5965b3347bfca1671737e07eb89c916cd2f
Alexander Cyon <alex.cyon@rdx.works>
2024-09-30 11:47:55 +0200

    YAGNI

--- src/recovery_securify_cache/helpers/mod.rs
+++ src/recovery_securify_cache/helpers/mod.rs
@@ -2,9 +2,6 @@ mod any_factor_derivation_request;
 mod any_factor_derivation_requests;
 mod derivation_analysis_types;
 mod factor_instances;
-mod is_derivation_done_query;
-mod on_chain_key_status_analyzer;
-mod profile_key_status_analyzer;
 mod unindex_derivation_request;
 mod unindex_derivation_requests;
 
@@ -12,8 +9,5 @@ pub use any_factor_derivation_request::*;
 pub use any_factor_derivation_requests::*;
 pub use derivation_analysis_types::*;
 pub use factor_instances::*;
-pub use is_derivation_done_query::*;
-pub use on_chain_key_status_analyzer::*;
-pub use profile_key_status_analyzer::*;
 pub use unindex_derivation_request::*;
 pub use unindex_derivation_requests::*;
