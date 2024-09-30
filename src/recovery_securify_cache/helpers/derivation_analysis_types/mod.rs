e905bda7a7dadc75aecd2fa8d8bce764a043bdf1
Alexander Cyon <alex.cyon@rdx.works>
2024-09-30 11:59:28 +0200

    wayni

--- src/recovery_securify_cache/helpers/derivation_analysis_types/mod.rs
+++ src/recovery_securify_cache/helpers/derivation_analysis_types/mod.rs
@@ -1,4 +1,3 @@
-mod derived_factor_instances;
 mod factor_instance_in_securified_space;
 mod factor_instance_in_unsecurified_space;
 mod probably_free_factor_instances;
@@ -6,7 +5,6 @@ mod securified_entity;
 mod unsecurified_entity;
 mod virtual_entity_creating_instances;
 
-pub use derived_factor_instances::*;
 pub use factor_instance_in_securified_space::*;
 pub use factor_instance_in_unsecurified_space::*;
 pub use probably_free_factor_instances::*;
