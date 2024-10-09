c073c16ed2c890ef34312e86d3f9cb29410d750c
Alexander Cyon <alex.cyon@gmail.com>
2024-10-09 12:18:07 +0200

    break out unit test into separate file

--- src/factor_instances_provider/provider/mod.rs
+++ src/factor_instances_provider/provider/mod.rs
@@ -2,6 +2,11 @@ mod cache;
 mod factor_instances_provider;
 mod outcome;
 
+#[cfg(test)]
+mod factor_instances_provider_unit_tests;
+#[cfg(test)]
+mod test_sargon_os;
+
 pub use cache::*;
 pub use factor_instances_provider::*;
 pub use outcome::*;
