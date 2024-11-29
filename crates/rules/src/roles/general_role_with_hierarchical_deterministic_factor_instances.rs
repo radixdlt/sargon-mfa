cb0ecdcdccb09187fde3e9c5a6343f055f288532
Alexander Cyon <alex.cyon@gmail.com>
2024-11-29 12:02:49 +0100

    more tests

--- crates/rules/src/roles/general_role_with_hierarchical_deterministic_factor_instances.rs
+++ crates/rules/src/roles/general_role_with_hierarchical_deterministic_factor_instances.rs
@@ -123,12 +123,35 @@ impl GeneralRoleWithHierarchicalDeterministicFactorInstances {
     }
 }
 
+impl HasSampleValues for GeneralRoleWithHierarchicalDeterministicFactorInstances {
+    fn sample() -> Self {
+        Self::try_from((MatrixOfFactorInstances::sample(), RoleKind::Primary))
+            .expect("Sample should not fail")
+    }
+
+    fn sample_other() -> Self {
+        Self::try_from((MatrixOfFactorInstances::sample_other(), RoleKind::Recovery))
+            .expect("Sample should not fail")
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[allow(clippy::upper_case_acronyms)]
     type SUT = GeneralRoleWithHierarchicalDeterministicFactorInstances;
+
+    #[test]
+    fn equality() {
+        assert_eq!(SUT::sample(), SUT::sample());
+        assert_eq!(SUT::sample_other(), SUT::sample_other());
+    }
+
+    #[test]
+    fn inequality() {
+        assert_ne!(SUT::sample(), SUT::sample_other());
+    }
 
     fn matrix() -> MatrixOfFactorInstances {
         MatrixOfFactorInstances::sample()
