9734ea38d0803bb45e9ca4b8a18c83766e6df22d
Alexander Cyon <alex.cyon@rdx.works>
2024-11-29 14:36:28 +0100

    split

---
+++ crates/rules/src/roles/factor_levels/mod.rs
@@ -0,0 +1,7 @@
+mod factor_instance_level;
+mod factor_source_id_level;
+mod factor_source_level;
+
+pub use factor_instance_level::*;
+pub use factor_source_id_level::*;
+pub(crate) use factor_source_level::*;
--- crates/rules/src/roles/mod.rs
+++ crates/rules/src/roles/mod.rs
@@ -1,13 +1,7 @@
 mod abstract_role_builder_or_built;
 mod builder;
-mod general_role_with_hierarchical_deterministic_factor_instances;
-mod role_with_factor_instances;
-mod roles_with_factor_ids;
-mod roles_with_factor_sources;
+mod factor_levels;
 
 pub(crate) use abstract_role_builder_or_built::*;
 pub use builder::*;
-pub use general_role_with_hierarchical_deterministic_factor_instances::*;
-pub(crate) use role_with_factor_instances::*;
-pub use roles_with_factor_ids::*;
-pub(crate) use roles_with_factor_sources::*;
+pub use factor_levels::*;
